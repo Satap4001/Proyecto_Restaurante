ff8844fe76f48b684c2bd3e68e692a5bdb0766e4
Dangt293 <danigt935@gmail.com>
2025-12-10 13:52:58 +0100

    Productos lista

--- productos.php
+++ productos.php
@@ -83,5 +83,4 @@
         echo "<td><form action = carrito.php><input type='hidden' value='".$producto['CodProd'] ."' name='codProduct'><input type='hidden' value='".$producto['Nombre'] ."' name='nombreProduct'><input type = 'number' name = 'numComprar'><button type = 'submit'>Comprar</button></form></td></tr>";
     };
     echo"</table></div>";
-    
 ?>
